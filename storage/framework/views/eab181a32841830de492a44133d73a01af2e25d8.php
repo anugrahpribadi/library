a2f5264633aa259d1962867652afc8b9f6bd699c
Anugrah Pribadi <anugrahpribadi09@gmail.com>
2022-04-09 14:34:44 +0700

    fix beranda

--- storage/framework/views/eab181a32841830de492a44133d73a01af2e25d8.php
+++ storage/framework/views/eab181a32841830de492a44133d73a01af2e25d8.php
@@ -1,11 +1,17 @@
 <?php $__env->startSection('content'); ?>
-
-<!-- <div class="col-md-3">
-    <div class="card">
-        <div class="card-header d-flex flex-column align-items-start pb-0">
-            <div class="avatar bg-rgba-primary p-50 m-0">
-                <div class="user-content">
-                    <i class="feather icon-book-open text-primary font-medium-5" button title="Jumlah Buku"></i>
+<div class="container-lg">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header d-flex flex-column align-items-start pb-0">
+                    <div class="avatar bg-rgba-primary p-50 m-0">
+                        <div class="user-content">
+                            <i class="feather icon-book-open text-primary font-medium-5" button title="Jumlah Buku"></i>
+                        </div>
+                    </div>
+                    <h2 class="text-bold-700 mt-1"><?php echo e($buku); ?></h2>
+                    <p class="mb-100">Total Buku</p>
+                    <br>
                 </div>
             </div>
         </div>
